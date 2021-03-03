b270eb1cfe24d5d6511e2d8e12f4e1ca6dc9f240
Andrey Voronkov <voronkovaa@gmail.com>
2021-03-03 21:29:02 +0300

    Tune a little

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,7 @@ $CFLAGS += " -std=c99 -Wno-declaration-after-statement"
 
 dir_config('lua5.4')
 
-unless have_library('lua5.4', 'luaL_newstate') or have_library('lua.5.4', 'luaL_newstate')
+unless have_library('lua5.4', 'luaL_newstate') || have_library('lua.5.4', 'luaL_newstate') || have_library('lua-5.4', 'luaL_newstate')
   puts ' extconf failure: need liblua5.4'
   exit 1
 end
