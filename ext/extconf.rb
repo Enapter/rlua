8d32cafcde4b02d1674e3509ef9ac1013b859a0c
Andrey Voronkov <voronkovaa@gmail.com>
2021-03-04 03:32:33 +0300

    Remove checks

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,9 +4,9 @@ $CFLAGS += " -std=c99 -Wno-declaration-after-statement"
 
 dir_config('lua5.3')
 
-unless have_library('lua5.3', 'luaL_newstate') or have_library('lua.5.3', 'luaL_newstate')
-  puts ' extconf failure: need liblua5.3'
-  exit 1
-end
+# unless have_library('lua5.3', 'luaL_newstate') or have_library('lua.5.3', 'luaL_newstate')
+#   puts ' extconf failure: need liblua5.3'
+#   exit 1
+# end
 
 create_makefile("rlua")
